caf3b39de0f3cc7f6ba8767c887ad084236729b8
xEsiah-LAPTOP <theo.thws.06@gmail.com>
2025-02-07 10:04:51 +0100

    Affichage pokémon

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
 import pokemon from "./data/pokemon_4x3.js";
 
 const liste_des_pokemons = pokemon;
+console.log("Variable liste_des_pokemons: \n", liste_des_pokemons);
 const liste_des_buissons = document.querySelectorAll(".bush");
-
-const liste_des_cases_de_la_grille =
-  document.querySelectorAll(".row row-cols-4");
-console.log(liste_des_cases_de_la_grille);
-
-liste_des_cases_de_la_grille.forEach((afficher_les_pokemons) => {
-  pokemon_choisi = generation_aleatoire_des_pokemons(liste_des_pokemons);
-  console.log(liste_des_pokemons);
-  console.log(pokemon_choisi);
-  afficher_les_pokemons.setAttribute();
-});
 
 liste_des_buissons.forEach((cacher_les_buissons) => {
   cacher_les_buissons.addEventListener("click", () => {
@@ -20,8 +10,22 @@ liste_des_buissons.forEach((cacher_les_buissons) => {
   });
 });
 
+console.log("Variable liste_des_buissons: ", liste_des_buissons);
+
+const liste_des_cases_de_la_grille = document.querySelectorAll(".box");
+
+console.log("liste_des_cases_de_la_grille \n", liste_des_cases_de_la_grille);
+
+liste_des_cases_de_la_grille.forEach((afficher_les_pokemons) => {
+  afficher_les_pokemons = generation_aleatoire_des_pokemons(liste_des_pokemons);
+  console.log(liste_des_pokemons);
+  afficher_les_pokemons.setAttribute(); // TROUVER ALTERNATIVE
+  console.log(afficher_les_pokemons);
+});
+
 function generation_aleatoire_des_pokemons(dictionnaire) {
   var pokemon_choisi_aléatoirement =
     dictionnaire[Math.floor(Math.random() * dictionnaire.length)];
+  console.log("Pokemon choisir par la fonction", pokemon_choisi_aléatoirement);
   return pokemon_choisi_aléatoirement;
 }
