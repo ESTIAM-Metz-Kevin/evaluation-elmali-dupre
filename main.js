666387164026fdc447b80552a31787297ec95ce2
xEsiah-LAPTOP <theo.thws.06@gmail.com>
2025-02-07 11:20:04 +0100

    Pokémons affichés

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import pokemon from "./data/pokemon_4x3.js";
 const liste_des_pokemons = pokemon;
 console.log("Variable liste_des_pokemons: \n", liste_des_pokemons);
 const liste_des_buissons = document.querySelectorAll(".bush");
+const liste_des_cases_de_la_grille = document.querySelectorAll(".box");
 
 liste_des_buissons.forEach((cacher_les_buissons) => {
   cacher_les_buissons.addEventListener("click", () => {
@@ -11,21 +12,25 @@ liste_des_buissons.forEach((cacher_les_buissons) => {
 });
 
 console.log("Variable liste_des_buissons: ", liste_des_buissons);
-
-const liste_des_cases_de_la_grille = document.querySelectorAll(".box");
-
 console.log("liste_des_cases_de_la_grille \n", liste_des_cases_de_la_grille);
-
-liste_des_cases_de_la_grille.forEach((afficher_les_pokemons) => {
-  afficher_les_pokemons = generation_aleatoire_des_pokemons(liste_des_pokemons);
-  console.log(liste_des_pokemons);
-  afficher_les_pokemons.setAttribute(); // TROUVER ALTERNATIVE
-  console.log(afficher_les_pokemons);
-});
 
 function generation_aleatoire_des_pokemons(dictionnaire) {
   var pokemon_choisi_aléatoirement =
     dictionnaire[Math.floor(Math.random() * dictionnaire.length)];
-  console.log("Pokemon choisir par la fonction", pokemon_choisi_aléatoirement);
   return pokemon_choisi_aléatoirement;
 }
+
+liste_des_cases_de_la_grille.forEach((caseElement) => {
+  let nouveau_pokémon = generation_aleatoire_des_pokemons(liste_des_pokemons);
+  console.log(nouveau_pokémon);
+  const { name, sprite } = nouveau_pokémon;
+  let nameElement = document.createElement("span");
+  nameElement.textContent = name;
+
+  let imgElement = document.createElement("img");
+  imgElement.src = sprite;
+  imgElement.alt = name;
+  caseElement.appendChild(imgElement);
+
+  console.log(`Pokémon affiché : ${name}`);
+});
