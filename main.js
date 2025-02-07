fc8b3453a23f09e6ad4c9b4fc62cf62ebda7427c
xEsiah-LAPTOP <theo.thws.06@gmail.com>
2025-02-07 11:48:32 +0100

    On click, cacher le buisson et afficher le pokémon

--- main.js
+++ main.js
@@ -5,9 +5,33 @@ console.log("Variable liste_des_pokemons: \n", liste_des_pokemons);
 const liste_des_buissons = document.querySelectorAll(".bush");
 const liste_des_cases_de_la_grille = document.querySelectorAll(".box");
 
+liste_des_cases_de_la_grille.forEach((caseElement) => {
+  let nouveau_pokémon = generation_aleatoire_des_pokemons(liste_des_pokemons);
+  console.log(nouveau_pokémon);
+  const { name, sprite } = nouveau_pokémon;
+  let nomElement = document.createElement("span");
+  nomElement.textContent = name;
+  let imgElement = document.createElement("img");
+  imgElement.src = sprite;
+  imgElement.className = "pokemon";
+  imgElement.style.display = "none";
+  imgElement.alt = name;
+  caseElement.appendChild(imgElement);
+  caseElement.addEventListener("click", () => {
+    imgElement.style.display = "";
+  });
+  console.log(`Pokémon affiché : ${name}`);
+});
+
 liste_des_buissons.forEach((cacher_les_buissons) => {
   cacher_les_buissons.addEventListener("click", () => {
     cacher_les_buissons.style.display = "none";
+  });
+});
+
+liste_des_cases_de_la_grille.forEach((afficher_les_pokemons) => {
+  afficher_les_pokemons.addEventListener("click", () => {
+    afficher_les_pokemons.style.display = "";
   });
 });
 
@@ -22,19 +46,4 @@ function generation_aleatoire_des_pokemons(dictionnaire) {
     dictionnaire[Math.floor(Math.random() * dictionnaire.length)];
   return pokemon_choisi_aléatoirement;
 }
-
-liste_des_cases_de_la_grille.forEach((caseElement) => {
-  let nouveau_pokémon = generation_aleatoire_des_pokemons(liste_des_pokemons);
-  console.log(nouveau_pokémon);
-  const { name, sprite } = nouveau_pokémon;
-  let nameElement = document.createElement("span");
-  nameElement.textContent = name;
-  let imgElement = document.createElement("img");
-  imgElement.src = sprite;
-  imgElement.className = "pokemon";
-  imgElement.style.zIndex = "auto";
-  imgElement.alt = name;
-  caseElement.appendChild(imgElement);
-
-  console.log(`Pokémon affiché : ${name}`);
-});
+function distribution_des_pokemons(grille) {}
