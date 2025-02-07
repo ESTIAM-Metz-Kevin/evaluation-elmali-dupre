dc9e05e20387dc3f9ffce83443e7a058b26a1b3d
xEsiah-LAPTOP <theo.thws.06@gmail.com>
2025-02-07 15:02:16 +0100

    Suppression console.log

--- main.js
+++ main.js
@@ -2,27 +2,27 @@ import pokemon from "./data/pokemon_4x3.js";
 
 const liste_des_cases_de_la_grille = document.querySelectorAll(".box");
 
-let nomsDesPokemons = pokemon.map((pokemon) => pokemon.name);
-
+let nomsDesPokemons = pokemon.map((pokemon) => pokemon);
 const listeDePaireDePokemon = nomsDesPokemons.concat(nomsDesPokemons);
 listeDePaireDePokemon.sort((pokemon) => 0.5 - Math.random());
-console.log(listeDePaireDePokemon);
 
 function createPokemon_ImageText(InfoPkmnACreer) {
-  const { name, sprite } = InfoPkmnACreer.name;
+  const { name, sprite } = InfoPkmnACreer;
+
   let nomElement = document.createElement("span");
   nomElement.textContent = name;
+  nomElement.style.display = "none";
 
   let imgElement = document.createElement("img");
-  imgElement.src = InfoPkmnACreer.sprite;
+  imgElement.src = sprite;
   imgElement.className = "pokemon";
-  imgElement.style.display = "none";
+  imgElement.style.display = "";
   imgElement.alt = name;
 
   let infos_parents = document.createElement("div");
   infos_parents.appendChild(nomElement);
   infos_parents.appendChild(imgElement);
-  console.log(infos_parents);
+
   return infos_parents;
 }
 
@@ -31,18 +31,21 @@ function giveMePokemonAt(liste_pair, index) {
   return Pokemon;
 }
 
-function clique_sur_evenement(liste_grille_element) {
+function clique_sur_evenement(liste_grille_element, liste_pokemon_random) {
+  // let utilisation_en_cours = true;
   liste_grille_element.forEach((box_element, index) => {
     box_element.addEventListener("click", () => {
+      // while utilisation_en_cours ==
       const bushImgElement = box_element.querySelector(".bush");
       bushImgElement.style.display = "none";
-      console.log("AHH");
+      // console.log("AHH");
       const imagePokemonElement = createPokemon_ImageText(
-        giveMePokemonAt(index)
+        giveMePokemonAt(liste_pokemon_random, index)
       );
       box_element.appendChild(imagePokemonElement);
+      console.log(imagePokemonElement);
     });
   });
 }
 
-clique_sur_evenement(liste_des_cases_de_la_grille);
+clique_sur_evenement(liste_des_cases_de_la_grille, listeDePaireDePokemon);
