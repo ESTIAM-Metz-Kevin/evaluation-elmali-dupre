fdfb28e0f763f5f91213654af9e19ed7d34b3728
xEsiah-LAPTOP <theo.thws.06@gmail.com>
2025-02-07 14:21:19 +0100

    Generation aléatoire des pokémons et création de fonctions pour afficher les sprite

--- main.js
+++ main.js
@@ -1,49 +1,48 @@
 import pokemon from "./data/pokemon_4x3.js";
 
-const liste_des_pokemons = pokemon;
-console.log("Variable liste_des_pokemons: \n", liste_des_pokemons);
-const liste_des_buissons = document.querySelectorAll(".bush");
 const liste_des_cases_de_la_grille = document.querySelectorAll(".box");
 
-liste_des_cases_de_la_grille.forEach((caseElement) => {
-  let nouveau_pokémon = generation_aleatoire_des_pokemons(liste_des_pokemons);
-  console.log(nouveau_pokémon);
-  const { name, sprite } = nouveau_pokémon;
+let nomsDesPokemons = pokemon.map((pokemon) => pokemon.name);
+
+const listeDePaireDePokemon = nomsDesPokemons.concat(nomsDesPokemons);
+listeDePaireDePokemon.sort((pokemon) => 0.5 - Math.random());
+console.log(listeDePaireDePokemon);
+
+function createPokemon_ImageText(InfoPkmnACreer) {
+  const { name, sprite } = InfoPkmnACreer.name;
   let nomElement = document.createElement("span");
   nomElement.textContent = name;
+
   let imgElement = document.createElement("img");
-  imgElement.src = sprite;
+  imgElement.src = InfoPkmnACreer.sprite;
   imgElement.className = "pokemon";
   imgElement.style.display = "none";
   imgElement.alt = name;
-  caseElement.appendChild(imgElement);
-  caseElement.addEventListener("click", () => {
-    imgElement.style.display = "";
-  });
-  console.log(`Pokémon affiché : ${name}`);
-});
 
-liste_des_buissons.forEach((cacher_les_buissons) => {
-  cacher_les_buissons.addEventListener("click", () => {
-    cacher_les_buissons.style.display = "none";
-  });
-});
-
-liste_des_cases_de_la_grille.forEach((afficher_les_pokemons) => {
-  afficher_les_pokemons.addEventListener("click", () => {
-    afficher_les_pokemons.style.display = "";
-  });
-});
-
-console.log("Variable liste_des_buissons: ", liste_des_buissons);
-console.log(
-  "Variable liste_des_cases_de_la_grille \n",
-  liste_des_cases_de_la_grille
-);
-
-function generation_aleatoire_des_pokemons(dictionnaire) {
-  var pokemon_choisi_aléatoirement =
-    dictionnaire[Math.floor(Math.random() * dictionnaire.length)];
-  return pokemon_choisi_aléatoirement;
+  let infos_parents = document.createElement("div");
+  infos_parents.appendChild(nomElement);
+  infos_parents.appendChild(imgElement);
+  console.log(infos_parents);
+  return infos_parents;
 }
-function distribution_des_pokemons(grille) {}
+
+function giveMePokemonAt(liste_pair, index) {
+  let Pokemon = liste_pair[index];
+  return Pokemon;
+}
+
+function clique_sur_evenement(liste_grille_element) {
+  liste_grille_element.forEach((box_element, index) => {
+    box_element.addEventListener("click", () => {
+      const bushImgElement = box_element.querySelector(".bush");
+      bushImgElement.style.display = "none";
+      console.log("AHH");
+      const imagePokemonElement = createPokemon_ImageText(
+        giveMePokemonAt(index)
+      );
+      box_element.appendChild(imagePokemonElement);
+    });
+  });
+}
+
+clique_sur_evenement(liste_des_cases_de_la_grille);
